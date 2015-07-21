5e290329eafa19912a3722671fa587218d7b7e91
Tushar Raibhandare <traib@users.noreply.github.com>
2015-07-21 12:53:45 +0530

    Use cURL instead of Wget

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ set -e
 
 CURDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-wget --no-verbose -O $CURDIR/zshrc https://raw.githubusercontent.com/grml/grml-etc-core/master/etc/zsh/zshrc 
+curl --output $CURDIR/zshrc https://raw.githubusercontent.com/grml/grml-etc-core/master/etc/zsh/zshrc 
 ln -sfn $CURDIR/zshrc ~/.zshrc
 ln -sfn $CURDIR/zshrc.local ~/.zshrc.local
